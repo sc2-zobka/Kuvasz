c971c6e5f00ba1a3f3b76ad2f80d5ad1b355a815
sc2-zobka <math.bocaz@gmail.com>
2021-07-26 01:43:48 -0400

    [FIX] Timezone validation:
    - All logic contained on method _check_days() will consider user's
    timezone.

--- kvz_time_sheet/models/timesheet.py
+++ kvz_time_sheet/models/timesheet.py
@@ -16,19 +16,20 @@ class TimeSheetInherited(models.Model):
         Type:
             fields.Date(): <class 'datetime.date'>
         """
-
+        
         # fetch model "ir.config_parameter" data
         data = self.env["ir.config_parameter"].sudo()
-
         # retrieve the value for "days_before_today" key
         days = int(data.get_param("days_before_today"))
 
-        today = fields.Date.today()
-        delta = today - self.date
-
+        # today's date with user context 
+        today_ctx = fields.Date.context_today(self)
+    
+        delta = today_ctx - self.date
+        
         if self.date:
-            if self.date > today:
-                raise ValidationError(_("Fecha no puede ser mayor a %s") % today)
+            if self.date > today_ctx:
+                raise ValidationError(_("Fecha no puede ser mayor a %s") % today_ctx)
 
-            elif delta.days > days:
+            if delta.days > days:
                 raise ValidationError(_("Fecha no permitida"))
